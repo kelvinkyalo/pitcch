816a1d848710211cd8ca5bdca1d6e0825820acc0
joan-w <wamuyujoan4@gmail.com>
2020-05-03 22:38:33 +0300

    Adding a pitches dictionary in a list that holds the pitches posted by users

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+pitches = [
+    {
+        'author' : 'Joan',
+        'category' : 'Promotion'
+        'content' : 'First pitch'
+        'date_posted' : 'Apri; 20, 2020'
+    },
+    {
+        'author' : 'Simon',
+        'category' : 'Interview'
+        'content' : 'Second pitch'
+        'date_posted' : 'Apri; 10, 2020'
+    },
+]
+
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', pitches=pitches)
 
 @app.route('/about')
 def about():
