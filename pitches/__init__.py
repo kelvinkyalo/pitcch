e5e38d53c7b72e221b8bb153b0a7ad7973f65cb7
joan-w <wamuyujoan4@gmail.com>
2020-05-05 01:49:44 +0300

    Importing the routes module

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -5,3 +5,5 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c1dd1056ef414905719940d2a9f3bef9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
+
+from pitches import routes
