32265e34ff0b3be349f2c087e177661560fa897f
joan-w <wamuyujoan4@gmail.com>
2020-05-05 01:45:35 +0300

    Removing the form imports

--- pitches/__init__.py
+++ pitches/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from forms import SignupForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c1dd1056ef414905719940d2a9f3bef9'
