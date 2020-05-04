344bcaf96d56e7f915c592e199010199a8b0947d
joan-w <wamuyujoan4@gmail.com>
2020-05-05 00:13:05 +0300

    Creating an instance of SQLALchemy

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = 'c1dd1056ef414905719940d2a9f3bef9'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 
+db = SQLAlchemy(app)
+
 pitches = [
     {
         'author' : 'Joan',
