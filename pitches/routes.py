e0ccc63f718f73b312c08e4a193719dac7506b6a
joan-w <wamuyujoan4@gmail.com>
2020-05-05 01:39:40 +0300

    Adding the routing code from the app file

---
+++ pitches/routes.py
@@ -0,0 +1,44 @@
+from models import User, Pitch
+
+pitches = [
+    {
+        'author' : 'Joan',
+        'category' : 'Promotion',
+        'content' : 'First pitch',
+        'date_posted' : 'April 30, 2020'
+    },
+    {
+        'author' : 'Simon',
+        'category' : 'Interview',
+        'content' : 'Second pitch',
+        'date_posted' : 'May 3, 2020'
+    },
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', pitches=pitches)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title = 'About Page')
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f'Account created for {form.username.data}!', 'success')
+        return redirect(url_for('home'))
+    return render_template('signup.html', title='Sign Up', form=form)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == 'wamuyu@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check your email and password.', 'danger')
+    return render_template('login.html', title='Login', form=form)
