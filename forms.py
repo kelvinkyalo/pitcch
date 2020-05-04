7a57b7248f9d6bd5545dec5a5d29132fc6192b7a
joan-w <wamuyujoan4@gmail.com>
2020-05-04 18:07:42 +0300

    Importing the wtf fields required for the forms in the app

--- forms.py
+++ forms.py
@@ -1,4 +1,4 @@
 from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Length, Email, EqualTo
 
-class SignupForm(FlaskForm):
-    username = StringField('Username')
