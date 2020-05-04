1ae653915743fc7fcd96d842e5b9a227ba425959
joan-w <wamuyujoan4@gmail.com>
2020-05-04 18:08:15 +0300

    Creating a SignupForm class that holds the signing up code

--- forms.py
+++ forms.py
@@ -2,3 +2,18 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length, Email, EqualTo
 
+class SignupForm(FlaskForm):
+    username = StringField('Username', 
+                validators=[DataRequired(), 
+                Length(min=2, max=20)])
+    email = StringField('Email',
+            validators=[DataRequired(),
+            Email()])
+    password = PasswordField('Password',
+                validators=[DataRequired(),
+                Length(8)])
+    confirm_password = PasswordField('Confirm Password',
+                        validators=[DataRequired(),
+                        EqualTo('password'),
+                        Length(8)])
+    submit = SubmitField('Sign Up')
