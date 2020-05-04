65c0712e27900fae5c915af1f7e940d3761a80ac
joan-w <wamuyujoan4@gmail.com>
2020-05-05 01:17:22 +0300

    Adding the models code from the app file

---
+++ models.py
@@ -0,0 +1,35 @@
+class User(db.Model):
+    id = db.Column(db.Integer,
+                    primary_key=True)
+    username = db.Column(db.String(20),
+                        unique = True,
+                        nullable =  False)
+    email = db.Column(db.String(120),
+                        unique = True,
+                        nullable =  False)
+    image_file = db.Column(db.String(20),
+                            nullable = False,
+                            default = 'default.jpg')
+    password = db.Column(db.String(60),
+                        nullable=False)
+    pitches = db.relationship('Pitch', backref='author', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+class Pitch(db.Model):
+    id = db.Column(db.Integer,
+                    primary_key=True)
+    category = db.Column(db.String,
+                        nullable=False)
+    date_posted = db.Column(db.DateTime,
+                            nullable=False,
+                            default=datetime.utcnow)
+    content = db.Column(db.Text,
+                        nullable=False)
+    user_id = db.Column(db.Integer,
+                        db.ForeignKey('user.id'),
+                        nullable=False)
+
+    def __repr__(self):
+        return f"Pitch('{self.category}', '{self.date_posted}')"
