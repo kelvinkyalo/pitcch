e94204b05c73574ab2048b8bbd93200e70d6ccd0
joan-w <wamuyujoan4@gmail.com>
2020-05-06 01:56:03 +0300

    Writing a function that saves the pictures a user uploads

--- pitches/routes.py
+++ pitches/routes.py
@@ -63,8 +63,14 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-# def save_picture(form_picture):
-#     random_hex = secrets.token_hex(8)
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', 'picture_fn')
+    form_picture.save(picture_path)
+
+    return picture_fn
 
 @app.route('/account', methods=['GET', 'POST'])
 @login_required
