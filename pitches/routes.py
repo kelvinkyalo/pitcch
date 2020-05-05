76cbf6aaf678cf3ad90d7faca163656a50369de4
joan-w <wamuyujoan4@gmail.com>
2020-05-06 00:28:05 +0300

    Importing an UpdateAccountClass from the forms file and creating a form variable in the login route

--- pitches/routes.py
+++ pitches/routes.py
@@ -1,6 +1,6 @@
 from flask import render_template, url_for, flash, redirect, request
 from pitches import app, db, bcrypt
-from pitches.forms import SignupForm, LoginForm
+from pitches.forms import SignupForm, LoginForm, UpdateAccountForm
 from pitches.models import User, Pitch
 from flask_login import login_user, current_user, logout_user, login_required
 
@@ -64,5 +64,7 @@ def logout():
 @app.route('/account')
 @login_required
 def account():
+    form = UpdateAccountForm()
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file)
+    return render_template('account.html', title='Account',
+                          image_file=image_file, form=form)
