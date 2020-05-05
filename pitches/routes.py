bd2bd9431839d57813f58b5af5eedf1b1444ef48
joan-w <wamuyujoan4@gmail.com>
2020-05-05 13:56:35 +0300

    Adding the information to the database and commiting it

--- pitches/routes.py
+++ pitches/routes.py
@@ -33,6 +33,8 @@ def signup():
     if form.validate_on_submit():
         hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
         flash(f'Account created for {form.username.data}!', 'success')
         return redirect(url_for('home'))
     return render_template('signup.html', title='Sign Up', form=form)
