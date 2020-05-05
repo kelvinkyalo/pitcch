b22590e910e685c94e51408d645e2ccc585056af
joan-w <wamuyujoan4@gmail.com>
2020-05-05 14:33:00 +0300

    Creating a function that checks whether a users email is unique

--- pitches/forms.py
+++ pitches/forms.py
@@ -4,32 +4,37 @@ from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationE
 from pitches.models import User
 
 class SignupForm(FlaskForm):
-    username = StringField('Username', 
+        username = StringField('Username', 
                 validators=[DataRequired(), 
                 Length(min=2, max=20)])
-    email = StringField('Email',
-            validators=[DataRequired(),
-            Email()])
-    password = PasswordField('Password',
+        email = StringField('Email',
+                validators=[DataRequired(),
+                Email()])
+        password = PasswordField('Password',
                 validators=[DataRequired(),
                 Length(min=8, max=16)])
-    confirm_password = PasswordField('Confirm Password',
+        confirm_password = PasswordField('Confirm Password',
                         validators=[DataRequired(),
                         EqualTo('password'),
                         Length(min=8, max=16)])
-    submit = SubmitField('Sign Up')
+        submit = SubmitField('Sign Up')
 
-    def validate_username(self, username):
-            user = User.query.filter_by(username=username.data).first()            
+        def validate_username(self, username):
+                user = User.query.filter_by(username=username.data).first()            
+                if user:
+                        raise ValidationError('That username is taken. Please choose another one.')
+
+        def validate_email(self, email):
+            user = User.query.filter_by(email=email.data).first()            
             if user:
-                    raise ValidationError('That username is taken. Please choose another one.')
+                    raise ValidationError('That email is taken. Please choose another one.')
 
 class LoginForm(FlaskForm):
-    email = StringField('Email',
-            validators=[DataRequired(),
-            Email()])
-    password = PasswordField('Password',
+        email = StringField('Email',
                 validators=[DataRequired(),
-                Length(8)])
-    remember = BooleanField('Remember me')
-    submit = SubmitField('Login')
+                Email()])
+        password = PasswordField('Password',
+                        validators=[DataRequired(),
+                        Length(8)])
+        remember = BooleanField('Remember me')
+        submit = SubmitField('Login')
