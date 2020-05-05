3e3fbd3c78744c74304fbd9f3ad1dfd53c6a47be
joan-w <wamuyujoan4@gmail.com>
2020-05-06 00:25:11 +0300

    Adding an UpdateAccountForm class that allows a user to update data to their account

--- pitches/forms.py
+++ pitches/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from pitches.models import User
@@ -38,3 +39,24 @@ class LoginForm(FlaskForm):
                         Length(8)])
         remember = BooleanField('Remember me')
         submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+        username = StringField('Username', 
+                validators=[DataRequired(), 
+                Length(min=2, max=20)])
+        email = StringField('Email',
+                validators=[DataRequired(),
+                Email()])
+        submit = SubmitField('Update')
+
+        def validate_username(self, username):
+                if username.data != current_user.username:
+                        user = User.query.filter_by(username=username.data).first()            
+                        if user:
+                                raise ValidationError('That username is taken. Please choose another one.')
+
+        def validate_email(self, email):
+                if email.data != current_user.email:
+                        user = User.query.filter_by(email=email.data).first()            
+                        if user:
+                                raise ValidationError('That email is taken. Please choose another one.')
