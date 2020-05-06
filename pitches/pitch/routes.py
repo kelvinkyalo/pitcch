0325ca6db640c528f02e9fe23c0ea7813d736b9b
joan-w <wamuyujoan4@gmail.com>
2020-05-06 23:26:15 +0300

    Changing the blueprints of the routes to pitch

---
+++ pitches/pitch/routes.py
@@ -0,0 +1,55 @@
+from flask import Blueprint
+
+pitch = Blueprint('pitch', __name__)
+
+
+@pitch.route("/pitch/new", methods=['GET', 'POST'])
+@login_required
+def new_pitch():
+    form = PitchForm()
+    if form.validate_on_submit():
+        pitch = Pitch(category=form.category.data, content=form.content.data, author=current_user)
+        db.session.add(pitch)
+        db.session.commit()
+        flash('Your pitch has been created!', 'success')
+        return redirect(url_for('home'))
+    return render_template('create_pitch.html', title='New Pitch',
+                           form=form, legend='New Pitch')
+
+
+@pitch.route("/pitch/<int:pitch_id>")
+def pitch(pitch_id):
+    pitch = Pitch.query.get_or_404(pitch_id)
+    return render_template('pitch.html', category=pitch.category, pitch=pitch)
+
+
+@pitch.route("/pitch/<int:pitch_id>/update", methods=['GET', 'POST'])
+@login_required
+def update_pitch(pitch_id):
+    pitch = Pitch.query.get_or_404(pitch_id)
+    if pitch.author != current_user:
+        abort(403)
+    form = PitchForm()
+    if form.validate_on_submit():
+        pitch.category = form.category.data
+        pitch.content = form.content.data
+        db.session.commit()
+        flash('Your pitch has been updated!', 'success')
+        return redirect(url_for('pitch', pitch_id=pitch.id))
+    elif request.method == 'GET':
+        form.category.data = pitch.category
+        form.content.data = pitch.content
+    return render_template('create_pitch.html', title='Update Pitch',
+                           form=form, legend='Update Pitch')
+
+
+@pitch.route("/pitch/<int:pitch_id>/delete", methods=['POST'])
+@login_required
+def delete_pitch(pitch_id):
+    pitch = Pitch.query.get_or_404(pitch_id)
+    if pitch.author != current_user:
+        abort(403)
+    db.session.delete(pitch)
+    db.session.commit()
+    flash('Your pitch has been deleted!', 'success')
+    return redirect(url_for('home'))
