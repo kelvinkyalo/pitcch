38c728eb793453b061ef8cbef8c29a288c038ca6
joan-w <wamuyujoan4@gmail.com>
2020-05-06 20:16:18 +0300

    Updating

--- pitches/models.py
+++ pitches/models.py
@@ -3,24 +3,18 @@ from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from pitches import db, login_manager, app
 from flask_login import UserMixin
 
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id))
 
+
 class User(db.Model, UserMixin):
-    id = db.Column(db.Integer,
-                    primary_key=True)
-    username = db.Column(db.String(20),
-                        unique = True,
-                        nullable =  False)
-    email = db.Column(db.String(120),
-                        unique = True,
-                        nullable =  False)
-    image_file = db.Column(db.String(20),
-                            nullable = False,
-                            default = 'default.jpg')
-    password = db.Column(db.String(60),
-                        nullable=False)
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)    
     pitches = db.relationship('Pitch', backref='author', lazy=True)
 
     def get_reset_token(self, expires_sec=1800):
@@ -35,7 +29,7 @@ class User(db.Model, UserMixin):
         except:
             return None
         return User.query.get(user_id)
-    
+
     def __repr__(self):
         return f"User('{self.username}', '{self.email}', '{self.image_file}')"
 
